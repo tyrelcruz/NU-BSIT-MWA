ebc941724dd93a5b80720802bd9f1e3f77e89476
Tyrel Cruz <111546308+tyrelcruz@users.noreply.github.com>
2023-03-29 14:59:59 +0800

    Create FileRead.java

---
+++ 1ST YEAR/2nd Term/NOTES/File Handling/FileRead.java
@@ -0,0 +1,23 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class FileRead {
+    public static void main(String[] args) throws FileNotFoundException {
+        File inputFile = new File("C:\\Users\\cruztp\\Documents\\NetBeansProjects\\Cruz_Programming\\input.txt");
+        Scanner inFile = new Scanner(inputFile);
+        
+        try{
+            while(inFile.hasNextLine()){
+            System.out.print(inFile.nextLine());
+            }
+            System.out.println("");
+        }
+        catch(Exception e){
+            System.out.println("An error has occured: "+e.getMessage());
+        }
+        finally{
+            inFile.close();
+        }
+    }
+}
