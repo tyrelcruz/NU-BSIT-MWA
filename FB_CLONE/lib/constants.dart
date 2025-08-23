d839ea0bd2cf2b354b5c2357773f6483679bdbe3
Tyrel Cruz <111546308+tyrelcruz@users.noreply.github.com>
2025-08-23 17:59:20 +0800

    New

--- FB_CLONE/lib/constants.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-const Color FB_PRIMARY = Color(0xFF0167f8);
-const Color FB_SECONDARY = Color(0xFF003bb3);
-const Color FB_DARK_PRIMARY = Color(0xFF021452);
-const Color FB_LIGHT_PRIMARY = Color(0xFF74b7fb);
-const Color FB_TEXT_COLOR_WHITE = Color(0xFFe0f0fd);
-const Color FB_TEXT_COLOR_BLACK = Color.fromARGB(255, 0, 0, 0);
